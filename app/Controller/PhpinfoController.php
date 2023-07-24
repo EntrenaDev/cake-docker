4afc4907c79626eb83cc0be81bc37fb1dd2768f3
EntrenaDev <mentrena@aicor.com>
2023-07-24 12:35:12 +0000

    First Commit

---
+++ app/Controller/PhpinfoController.php
@@ -0,0 +1,12 @@
+<?php
+
+App::uses('Controller', 'Controller');
+
+class PhpinfoController extends Controller
+{
+    public function index()
+    {
+        // Muestra la información de phpinfo.
+        /* return phpinfo(); */
+    }
+}
---
+++ docker/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Cambiar propietario al grupo www-data
+# sudo chown -R :www-data tmp
+
+# Establecer permisos en el directorio
+# sudo chmod -R 770 tmp
